7fdd238adc05f298f3715e8c1c5a53ae700559b7
samat <samat@method.kz>
2017-06-28 06:42:14 +0000

    add actions show,edit ,destroy to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-    
+    before_action :set_article, only: [:show, :edit,:update,:destroy]
     def index
         @articles = Article.all
     end
@@ -10,14 +10,35 @@ class ArticlesController < ApplicationController
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
         if @article.save
+            flash[:notice] = "Пост успешно создан!"
           redirect_to articles_path  
         else
             render "new"
         end
-        
+    end
+    
+    def show
+    end
+    def edit
+    end
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Пост обнoвлен!"
+            redirect_to article_path(@article)
+        else
+            render "edit"
+        end
+    end
+    def destroy
+        @article.destroy
+        flash[:notice] = ": ( где мой пост"
+        redirect_to articles_path
     end
     private
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+    def set_article
+        @article = Article.find(params[:id])
     end
 end
